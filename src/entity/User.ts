3dd2dcc26206d3f620da7f1218fd3273615b6de3
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-02 09:06:46 +0000

    made changes to model and added the deletedAt

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
 
-@Entity()
+@Entity({ name: "users" })
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: "varchar", length: 255 })
   firstName: string;
 
-  @Column()
+  @Column({ type: "varchar", length: 255 })
   lastName: string;
 
-  @Column()
+  @Column({ type: "int4" })
   age: number;
+
+  @CreateDateColumn({ type: "timestamp" })
+  public createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  public updatedAt: Date;
+
+  @DeleteDateColumn()
+  public deletedAt: Date;
 }
