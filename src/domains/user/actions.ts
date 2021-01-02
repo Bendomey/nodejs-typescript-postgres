2edd2903fd85f2a5b025dab5b448c293e4260b9a
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-02 08:16:46 +0000

    db related actions for user

---
+++ src/domains/user/actions.ts
@@ -0,0 +1,40 @@
+import {
+  ICreateUser,
+  IGetSingleUser,
+  IRemoveUser,
+  IUpdateUser,
+} from "../../@Types/user";
+import { User } from "../../entity/User";
+
+const all = () => {
+  return User.find();
+};
+
+const one = async (body: IGetSingleUser) => {
+  const __oldUser = await User.findOne(body.id);
+  if (!__oldUser) return new Error("UserNotFound");
+
+  return __oldUser;
+};
+
+const save = async (body: ICreateUser) => {
+  const __newUser = User.create(body);
+  await __newUser.save();
+  return __newUser;
+};
+
+const update = async (body: IUpdateUser) => {
+  const __oldUser = await User.findOne(body.id);
+  if (!__oldUser) return new Error("UserNotFound");
+  await User.update({ id: __oldUser.id }, { ...body });
+  return __oldUser;
+};
+
+const remove = async (body: IRemoveUser) => {
+  const __oldUser = await User.findOne(body.id);
+  if (!__oldUser) return new Error("UserNotFound");
+  await __oldUser.softRemove();
+  return true;
+};
+
+export { all, one, save, update, remove };
